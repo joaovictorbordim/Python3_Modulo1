b6e67afb01d8127644be2c3c52f3f5a89fe7bae9
joaovictorbordim <joaobordimalves@gmail.com>
2025-01-14 16:54:39 -0300

    14/01/2025 - 16:54

---
+++ exercícios/aula06_ex04.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela.
+
+n = input('Digite algo para saber informações técnica sobre os dados de entrada. ')
+print(type(n))
+print(n.isalnum())
+print(n.isalpha())
