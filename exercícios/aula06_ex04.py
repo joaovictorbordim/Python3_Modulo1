1b4ba4f3408a9c63de0c27e5cfb8003597de4802
joaovictorbordim <joaobordimalves@gmail.com>
2025-01-14 17:34:06 -0300

    14/01/2025 - 16:54

--- exercícios/aula06_ex04.py
+++ exercícios/aula06_ex04.py
@@ -1,6 +1,9 @@
 #Crie um programa que leia pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela.
 
-n = input('Digite algo para saber informações técnica sobre os dados de entrada. ')
-print(type(n))
-print(n.isalnum())
-print(n.isalpha())
+n = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(n))
+print('Só tem espaço? ', n.isspace())
+print('É número? ' ,n.isnumeric())
+print('É alfabeto? ', n.isalpha())
+print('Está em maiúscula? ', n.isupper())
+print('Está em minúscula? ', n.islower())
